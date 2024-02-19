6705f6078a27a7b213703d3baedfb5512e0c7d6b
Rawwu <62627461+Rawwu@users.noreply.github.com>
2024-02-18 21:09:24 -0600

    Backend files and directories
    Added 
    + package.json 
    + server.js
    + models directory
        +flight.model.js
    + routes
        +flights.js

---
+++ models/flight.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+
+const Schema = mongoose.Schema;
+
+
+const flightSchema = new Schema({
+    _id: { type: Schema.Types.ObjectId, required: true },
+    airline: {
+      id: { type: Number, required: true },
+      name: { type: String, required: true },
+      alias: { type: String, required: true },
+      iata: { type: String, required: true }
+    },
+    src_airport: { type: String, required: true },
+    dst_airport: { type: String, required: true },
+    codeshare: { type: String, required: false },
+    stops: { type: Number, required: true },
+    airplane: { type: String, required: true }
+  }, {
+    timestamps: true,
+  });
+
+const Flights = mongoose.model('Flights', flightSchema);
+
+
+module.exports = Flights;
+
+/*
+{"_id":{"$oid":"56e9b39b732b6122f877fa31"},
+"airline":{
+    "id":{"$numberInt":"410"},
+    "name":"Aerocondor",
+    "alias":"2B",
+    "iata":"ARD" },
+"src_airport":"CEK",
+"dst_airport":"KZN",
+"codeshare":"",
+"stops":{"$numberInt":"0"},
+"airplane":"CR2"}
+*/
