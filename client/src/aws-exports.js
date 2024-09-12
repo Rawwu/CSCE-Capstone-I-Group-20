c5b57fd219e58c5d926e1ebedbde5ce4d3671d7d
Rawwu <62627461+Rawwu@users.noreply.github.com>
2024-09-12 17:30:46 -0500

    Added aws-exports.js file
    File contains configurations for AWS cognito

---
+++ client/src/aws-exports.js
@@ -0,0 +1,11 @@
+// src/aws-exports.js
+const awsExports = {
+    Auth: {
+      region: 'us-east-2', // Your region
+      userPoolId: 'us-east-2_U0diJv929',
+      userPoolWebClientId: '69g0o5hvmc78i0olmmktooho42',
+    }
+  };
+  
+  export default awsExports;
+  
