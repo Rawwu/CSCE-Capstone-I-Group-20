b269a3ea6203acdb3b9d9ab167130a3e62253cbc
Rawwu <62627461+Rawwu@users.noreply.github.com>
2024-09-16 15:13:01 -0500

    Update aws-exports.js
    Deleted oauth fields.

--- client/src/aws-exports.js
+++ client/src/aws-exports.js
@@ -3,13 +3,6 @@ const awsExports = {
     region: 'us-east-2',
     userPoolId: 'us-east-2_U0diJv929',
     userPoolWebClientId: '69g0o5hvmc78i0olmmktooho42',
-    oauth: {
-      domain: '',  // Empty domain to disable OAuth
-      scope: [],
-      redirectSignIn: '',
-      redirectSignOut: '',
-      responseType: '', // Empty response type to disable OAuth
-    }
   }
 };
 
