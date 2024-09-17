abb9e53e990fe19df474c90d80bc9444b5e291c7
Rawwu <62627461+Rawwu@users.noreply.github.com>
2024-09-17 14:16:16 -0500

    Update aws-exports.js
    Added Cognito object to fix errors

--- client/src/aws-exports.js
+++ client/src/aws-exports.js
@@ -1,18 +1,14 @@
 const awsExports = {
   Auth: {
-    region: 'us-east-2',
-    userPoolId: 'us-east-2_U0diJv929',
-    userPoolWebClientId: '69g0o5hvmc78i0olmmktooho42',
-    mandatorySignIn: false, // This is optional, depending on app's needs
-    authenticationFlowType: 'USER_SRP_AUTH', // If using standard authentication with Cognito
+    Cognito: {
+        region: 'us-east-2',
+        userPoolId: 'us-east-2_U0diJv929',
+        userPoolWebClientId: '69g0o5hvmc78i0olmmktooho42',
+        mandatorySignIn: false, // This is optional, depending on app's needs
+        authenticationFlowType: 'USER_SRP_AUTH', // If using standard authentication with Cognito
+    }
   },
-  oauth: {
-    domain: '', // Leave it empty if not using OAuth
-    scope: [],
-    redirectSignIn: '',
-    redirectSignOut: '',
-    responseType: 'code', // Optional, but set to 'code' if using Authorization Code Flow
-  }
+  oauth: null //Disable OAuth
 };
 
 export default awsExports;
