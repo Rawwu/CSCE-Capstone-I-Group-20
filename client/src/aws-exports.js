dbce52fe08721532f7dde92c81cae3371153cfa7
Rawwu <62627461+Rawwu@users.noreply.github.com>
2024-09-17 11:39:39 -0500

    Update aws-exports.js
    Added extra fields to prevent oauth error.

--- client/src/aws-exports.js
+++ client/src/aws-exports.js
@@ -3,6 +3,15 @@ const awsExports = {
     region: 'us-east-2',
     userPoolId: 'us-east-2_U0diJv929',
     userPoolWebClientId: '69g0o5hvmc78i0olmmktooho42',
+    mandatorySignIn: false, // This is optional, depending on app's needs
+    authenticationFlowType: 'USER_SRP_AUTH', // If using standard authentication with Cognito
+  },
+  oauth: {
+    domain: '', // Leave it empty if not using OAuth
+    scope: [],
+    redirectSignIn: '',
+    redirectSignOut: '',
+    responseType: 'code', // Optional, but set to 'code' if using Authorization Code Flow
   }
 };
 
