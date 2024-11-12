7b8257753172fc999baf403c64d421690fb0e006
Rawwu <62627461+Rawwu@users.noreply.github.com>
2024-11-12 15:59:39 -0600

    Updated airline logos routes for additional logo images.

--- client/src/assets/airlineLogos.js
+++ client/src/assets/airlineLogos.js
@@ -1,9 +1,12 @@
 const airlineLogos = {
-    "NK": "/images/logos/NK.png",
-    "B6": "/images/logos/B6.png",
-    "UA": "/images/logos/UA.png",
-    "TK": "/images/logos/TK.png",
-    "F9": "/images/logos/F9.png"
-  };
-  
-  export default airlineLogos;
+  "NK": "/images/logos/NK.png",
+  "B6": "/images/logos/B6.png",
+  "UA": "/images/logos/UA.png",
+  "TK": "/images/logos/TK.png",
+  "F9": "/images/logos/F9.png",
+  "AS": "/images/logos/AS.png",
+  "6X": "/images/logos/6X.jpg",
+  "9K": "/images/logos/9K.png"
+};
+
+export default airlineLogos;
