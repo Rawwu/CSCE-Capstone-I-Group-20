ea721e8b6ff33fbd8c1093ef9bcdcd6a51b1eac9
Rawwu <62627461+Rawwu@users.noreply.github.com>
2024-11-12 19:30:59 -0600

    Added logo image route for AC (Air Canada)

--- client/src/assets/airlineLogos.js
+++ client/src/assets/airlineLogos.js
@@ -6,7 +6,8 @@ const airlineLogos = {
   "F9": "/images/logos/F9.png",
   "AS": "/images/logos/AS.png",
   "6X": "/images/logos/6X.jpg",
-  "9K": "/images/logos/9K.png"
+  "9K": "/images/logos/9K.png",
+  "AC": "/images/logos/AC.png"
 };
 
 export default airlineLogos;
